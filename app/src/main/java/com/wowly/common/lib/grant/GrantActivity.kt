eb187e4ae141bab2705b68f0ef095c70f4c69e14
lion <zqhusixia@163.com>
2021-04-06 15:23:15 +0800

    add grant lib

---
+++ app/src/main/java/com/wowly/common/lib/grant/GrantActivity.kt
@@ -0,0 +1,12 @@
+package com.wowly.common.lib.grant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wowly.common.lib.grant.R.*
+
+class GrantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
